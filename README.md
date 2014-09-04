4c155587a8d5fd7b6c4f6c7417efcdf77a5921fb
Evan2a <zaiwei923@gmail.com>
2014-09-04 10:30:48 +0800

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-localStorage
+# localStorage
 ============
+> 使用userData模拟localStorage对IE6/7进行支持
 
-localStorage
+```js
+var ls = toolkit.localStorage;
+
+if( ls.getItem('test') != null ){
+    ls.setItem('test', 'Hello, Evan!');
+}
+
+ls.getItem('test');
+```
+
+## API
+
+> getItem( key )
+
+> setItem( key, value )
+
+> removeItem( key )
+
+> key( index )
+
+> clear()
---
+++ localStorage.js
@@ -0,0 +1,255 @@
+/*
+ * @description 封装localstorage，IE6/7下只能存取于同一路径下的文件，单个文件大小不得大于128k，key无法区分大小写。firefox 14+去掉了globalStorage
+ * @author Evan(zaiwei923@gmail.com)
+ * @date 2014/9/3
+ * @time 17:23
+*/
+
+(function( window ){
+    "use strict";
+
+var toolkit = window.toolkit || {};
+
+if( toolkit.localStorage ){
+    return;
+}
+
+var wls = window.localStorage;
+
+if( wls ){
+
+    toolkit.localStorage = {
+        length: wls.length,
+
+        setItem: function ( key, value) {
+            if( key === '' || key == null || arguments.length < 2 ){
+                return ;
+            }
+
+            wls.setItem(key, value);
+            this.length = wls.length;
+        },
+
+        getItem: function ( key ) {
+            if( key === '' || key == null ){
+                return ;
+            }
+
+            return wls.getItem( key );
+        },
+
+        removeItem: function ( key ) {
+            if( key === '' || key == null ){
+                return ;
+            }
+
+            wls.removeItem( key );
+            this.length = wls.length;
+        },
+
+        key: function ( index ) {
+            var index = +index;
+            if( !isFinite(index) ){
+                return ;
+            }
+
+            return wls.key( index );
+        },
+
+        clear: function () {
+            wls.clear();
+            this.length = 0;
+        }
+    }
+
+} else {
+
+    var name = location.hostname || '__storage',  //用于缓存主要数据
+        cache = '__cache__',  //用于缓存key list
+        prefix = '_pf_', 
+        __storage = document.createElement('style'); 
+
+    __storage.addBehavior('#default#userData');
+    document.getElementsByTagName('head')[0].appendChild(__storage);
+
+    /*
+     * @fn 增加一个key
+     * @param {key}
+     * @private
+    */
+    var addKey = function ( key ) {
+        __storage.load(cache);
+        var value = __storage.getAttribute(cache);
+
+        if( value == null ){
+            __storage.setAttribute(cache, key);
+            __storage.save(cache);
+        } else {
+            if( value.indexOf(key) > -1 ){
+                return ;
+            } else {
+                __storage.setAttribute(cache, value + '|' + key);
+                __storage.save(cache);
+            }
+        }
+        value = __storage.getAttribute(cache);
+        toolkit.localStorage.length = value.split('|').length;
+    }
+
+    /*
+     * @fn 删除某个key
+     * @param {key}
+     * @private
+    */
+    var removeKey = function ( key ) {
+        __storage.load(cache);
+        var value = __storage.getAttribute(cache);
+
+        if( value == null || value.indexOf(key) === -1 ){
+            return ;
+        }
+
+        var list = value.split('|'),
+            count = 0,
+            length = list.length;
+
+        for(; count < length; count++){
+            if( list[count] === key ){
+                list.splice(count, 1);
+                __storage.setAttribute(cache, list.join('|'));
+                __storage.save(cache);
+                break;
+            }
+        }
+
+        if( list + '' === '' ){
+            __storage.removeAttribute(cache);
+            toolkit.localStorage.length = 0;
+            __storage.save(cache);
+        } else {
+            toolkit.localStorage.length = list.length;
+        }
+    }
+
+    /*
+     * @fn 获取本地存储中的所有key
+     * @return {array} 
+     * @private
+    */
+    var getAllKey = function () {
+        
+        __storage.load(cache);
+        var value = __storage.getAttribute(cache),
+            ret = [];
+        
+        if( value == null ){
+            return ret;
+        }
+
+        return (value + '').split('|');
+    }
+
+    /*
+     * @fn 刷新本地存储长度
+     * @private
+    */
+    var refreshLength = function(){
+        __storage.load(cache);
+        var ret = __storage.getAttribute(cache);
+
+        if( ret == null ){
+            toolkit.localStorage.length = 0;
+        } else {
+            toolkit.localStorage.length = ret.split('|').length;
+        }
+    }
+
+    toolkit.localStorage = {
+        length: 0,
+
+        /*
+         * @method localStorage setItem
+         * @public
+        */
+        setItem: function ( key, value ) {
+            if( key === '' || key == null || arguments.length < 2 ){
+                return ;
+            }
+            __storage.load(name);
+            __storage.setAttribute(prefix + key, value);
+            __storage.save(name);
+            addKey(key);
+        },
+
+        /*
+         * @method localStorage getItem
+         * @public
+        */
+        getItem: function ( key ) {
+            if( key === '' || key == null ){
+                return ;
+            }
+            __storage.load(name);
+            return __storage.getAttribute(prefix + key );
+        },
+
+        /*
+         * @method localStorage removeItem
+         * @public
+        */
+        removeItem: function ( key ) {
+            if( key === '' || key == null ){
+                return ;
+            }
+            __storage.load(name);
+            __storage.removeAttribute(prefix + key );
+            __storage.save(name);
+            removeKey(key);
+        },
+
+        /*
+         * @method localStorage clear
+         * @public
+        */
+        clear: function () {
+            var ret = getAllKey();
+
+            __storage.removeAttribute(cache);
+            __storage.save(cache);
+
+            __storage.load(name);
+            for( var count = 0, key; key = ret[count++]; ){
+                __storage.removeAttribute(prefix + key);
+            }
+            __storage.save(name);
+            this.length = 0;
+        },
+
+        /*
+         * @method localStorage key
+         * @public
+        */
+        key: function ( index ) {
+            var index = +index;
+            if( !isFinite(index) ){
+                return ;
+            }
+
+            var ret = getAllKey(),
+                curKey = ret[index];
+
+            if( curKey == null ){
+                return ;
+            }
+            __storage.load(name);
+            return __storage.getAttribute(prefix + curKey);
+        }
+    }
+    
+    // 初始化一次length
+    refreshLength();
+}
+
+window.toolkit = toolkit;
+
+})( window );
